9a1e1b4a5addfdaa5234a8717849561842765609
CUFRED <cufred@aprimail.com>
2021-03-03 00:14:16 +0400

    Messeg

--- index.php
+++ index.php
@@ -3,28 +3,40 @@
 <div class="container">
     <div class="row">
         <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-            <!-- post -->
+        <!-- post -->
 
-            <div class="col-md-12">
-                <div class="card">
+        <div class="col-md-12">
+            <div class="card">
+                <div class="card-header">
+                    <h5 class="card-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h5>
+                </div>
+
+                <div class="card-body">
                     <?php if(has_post_thumbnail()): ?>
-                        <?php the_post_thumbnail('thumbnail');?>
+                    <?php the_post_thumbnail('thumbnail', array('class' => 'float-left mr-3'));?>
                     <?php else: ?>
-                            <img src="https://picsum.photos/150/150" width="150" height="150" alt="">
+                    <img src="https://picsum.photos/150/150" width="150" height="150" alt="" class="float-left mr-3">
                     <?php endif; ?>
-                    <div class="card-body">
-                        <h5 class="card-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h5>
-                        <p class="card-text"><?php the_excerpt(); //the_content(''); ?></p>
-                        <a href="<?php the_permalink(); ?>"class="btn btn-primary">Go somewhere</a>
-                    </div>
+                    <p class="card-text"><?php the_excerpt(); //the_content(''); ?></p>
+                </div>
+
+                <div class="card-footer">
+                    <a href="<?php the_permalink(); ?>" class="btn btn-primary">Go somewhere</a>
                 </div>
             </div>
+        </div>
 
         <?php endwhile; ?>
-            <!-- post navigation -->
+        <!-- post navigation -->
+        <?php the_posts_pagination(array(
+                'show_all'     => false,
+                'end_size'     => 1,
+                'mid_size'     => 2,
+                'type'         => 'list'
+        )); ?>
         <?php else: ?>
-            <!-- no posts found -->
-            <p>Постов нет...</p>
+        <!-- no posts found -->
+        <p>Постов нет...</p>
         <?php endif; ?>
     </div>
 </div>
